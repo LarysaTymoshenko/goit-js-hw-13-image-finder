0c21ef1db67f6ba505b8434c669ec4cc6552371a
LarysaTymoshenko <luda76@ukr.net>
2021-10-27 00:53:57 +0300

    053

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './sass/main.scss';
 import './js/apiService.js';
 import './js/lightbox.js';
-// import './js/gallery.js';
+import './js/refs.js';
 import './js/master.js';
 
--- src/js/refs.js
+++ src/js/refs.js
@@ -1,5 +1,13 @@
-export default {
-  formEl: document.querySelector('.search-form'),
-  galleryEl: document.querySelector('.gallery'),
+// export default {
+//   formEl: document.querySelector('.search-form'),
+//   galleryEl: document.querySelector('.gallery'),
  
-}
+// }
+export default {
+  bodyEL: document.querySelector('.body'),
+  galleryEL: document.querySelector('.gallery'),
+  inputEL: document.getElementById('input'),
+  formEL: document.getElementById('search-form'),
+  loadMoreBtnEL: document.querySelector('.gallery__load-btn'),
+  sentinelEL: document.querySelector('.sentinel'),
+};
